a9efbfd7bfc2f725523342337e9f0a6d4477b8a6
abhishekkr.ug21.ec@nitp.ac.in <121070042+abhishekkrug9472@users.noreply.github.com>
2024-07-13 20:05:52 +0530

    added url of fronted

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -17,7 +17,7 @@ const Contact = () => {
       if (!name || !email || !msg) {
         toast.error("PLease Provide all fields");
       }
-      const res = await axios.post("http://localhost:8080/api/v1/portfolio/sendEmail", {
+      const res = await axios.post("https://mern-portfolio1.onrender.com/api/v1/portfolio/sendEmail", {
         name,
         email,
         msg,
